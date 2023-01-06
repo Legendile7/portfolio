9fd667b694c44493ade32f02dfed7b355cae4e74
Divesh Gupta <diveshg9@gmail.com>
2023-01-06 10:12:40 -0500

    Update data.tsx
    Signed-off-by: Divesh Gupta <diveshg9@gmail.com>

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -12,12 +12,12 @@ import GithubIcon from '../components/Icon/GithubIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
 import TwitterIcon from '../components/Icon/TwitterIcon';
 import heroImage from '../images/header-background.webp';
-import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
-import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
-import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
-import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
-import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
-import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
+import porfolioImage1 from '../images/portfolio/gridshift.png';
+import porfolioImage2 from '../images/portfolio/commonground.png';
+import porfolioImage3 from '../images/portfolio/osgWebsite.png';
+import porfolioImage4 from '../images/portfolio/astroflight.png';
+import porfolioImage5 from '../images/portfolio/originsScreenshot.png';
+import porfolioImage6 from '../images/portfolio/Marooned.png';
 import profilepic from '../images/profilepic.jpg';
 import testimonialImage from '../images/testimonial.webp';
 import {
